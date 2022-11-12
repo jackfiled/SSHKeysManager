d709240c00eca05054588bac6abb6302a81c13f0
jackfiled <xcrenchangjun@outlook.com>
2022-11-12 23:39:00 +0800

    迁移数据库架构 添加了用户密码和用户权限

---
+++ SSHKeysManager/Migrations/20221112152354_AddUserPasswordAndPermission.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SSHKeysManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserPasswordAndPermission : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Permission",
+                table: "Users",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Permission",
+                table: "Users");
+        }
+    }
+}
